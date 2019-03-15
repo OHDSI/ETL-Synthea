5dfb80789469376f5a2122bd30eaa253c5b98256
Clair Blacketer <clair.blacketer@gmail.com>
2019-03-15 15:30:44 -0400

    Additional test cases

---
+++ extras/ObservationPeriodTests.r
@@ -0,0 +1,12 @@
+createObservationPeriodTests <- function () {
+
+	patient <- createPatient()
+	encounter1 <- createEncounter()
+	encounter2 <- createEncounter()
+	declareTest(id = patient$id, description = "Test that observation period is taking the earliest start and latest stop, id is person_source_value")
+	add_patients(id = patient$id, gender = NULL)
+	add_encounters(id = encounter1$id, patient = patient$id, encounterclass = "outpatient", start = "2009-10-19", stop = "2009-10-19")
+	add_encounters(id = encounter2$id, patient = patient$id, encounterclass = "inpatient", start = "2009-11-21", stop = "2009-11-30")
+	expect_observation_period(person_source_value = patient$id, observation_period_start_date = "2009-10-19", observation_period_end_date = "2009-11-30")
+
+	}
