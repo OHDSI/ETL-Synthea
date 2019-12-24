70fd5b4a0aad13d7f3f9272dea91f75d97f8a4a9
Joris Borgdorff <github@blootsvoets.net>
2019-12-24 14:39:37 +0100

    Load vocabularies separately from ETL

---
+++ R/DropVocabViews.r
@@ -0,0 +1,39 @@
+#' @title Drop Vocabulary Views.
+#'
+#' @description This function drops Vocabulary views.
+#'
+#' @usage DropVocabViews(connectionDetails,vocabDatabaseSchema)
+#'
+#' @param connectionDetails  An R object of type\cr\code{connectionDetails} created using the
+#'                                     function \code{createConnectionDetails} in the
+#'                                     \code{DatabaseConnector} package.
+#' @param vocabDatabaseSchema  The name of the database schema that contains the Vocabulary
+#'                                     instance.  Requires read and write permissions to this database. On SQL
+#'                                     Server, this should specifiy both the database and the schema,
+#'                                     so for example 'vocab_instance.dbo'.
+#'
+#'@export
+
+
+DropVocabViews <- function (connectionDetails, vocabDatabaseSchema)
+{
+
+
+    pathToSql <- base::system.file("sql/sql_server", package = "ETLSyntheaBuilder")
+
+    sqlFile <- base::paste0(pathToSql, "/", "drop_vocab_views.sql")
+
+    sqlQuery <- base::readChar(sqlFile, base::file.info(sqlFile)$size)
+
+    renderedSql <- SqlRender::render(sqlQuery, vocab_schema = vocabDatabaseSchema)
+
+    translatedSql <- SqlRender::translate(renderedSql, targetDialect = connectionDetails$dbms)
+
+    writeLines("Running drop_vocab_views.sql")
+
+    conn <- DatabaseConnector::connect(connectionDetails)
+
+    DatabaseConnector::dbExecute(conn, translatedSql, progressBar = TRUE, reportOverallTime = TRUE)
+
+    on.exit(DatabaseConnector::disconnect(conn))
+}
---
+++ inst/sql/sql_server/drop_vocab_views.sql
@@ -0,0 +1,10 @@
+DROP VIEW @cdm_schema.concept;
+DROP VIEW @cdm_schema.concept_relationship;
+DROP VIEW @cdm_schema.concept_ancestor;
+DROP VIEW @cdm_schema.concept_synonym;
+DROP VIEW @cdm_schema.concept_class;
+DROP VIEW @cdm_schema.domain;
+DROP VIEW @cdm_schema.vocabulary;
+DROP VIEW @cdm_schema.relationship;
+DROP VIEW @cdm_schema.drug_strength;
+DROP VIEW @cdm_schema.source_to_concept_map;
