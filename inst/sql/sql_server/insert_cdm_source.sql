71490b9e518f314e731d71702be9ca6bd521f5a7
AnthonyMolinaro <31700317+AnthonyMolinaro@users.noreply.github.com>
2021-01-08 13:11:03 -0500

    updated insert to cdm_source to pull vocab version from the vocabulary table, to address issue #75

--- inst/sql/sql_server/insert_cdm_source.sql
+++ inst/sql/sql_server/insert_cdm_source.sql
@@ -9,15 +9,17 @@ source_release_date,
 cdm_release_date,
 cdm_version,
 vocabulary_version
-) values (
+) 
+select
 'Synthea synthetic health database',
 'Synthea',
 'OHDSI Community',
 'SyntheaTM is a Synthetic Patient Population Simulator. The goal is to output synthetic, realistic (but not real), patient data and associated health records in a variety of formats.',
 'https://synthetichealth.github.io/synthea/',
 'https://github.com/OHDSI/ETL-Synthea',
-'2019-10-02',
-'2019-10-07',
+getdate(), -- NB: Set this value to the day the source data was pulled
+getdate(), 
 'v6.0',
-'v5.0 17-JUN-19'
-);
+vocabulary_version
+from @cdm_schema.vocabulary 
+where vocabulary_id = 'None';
