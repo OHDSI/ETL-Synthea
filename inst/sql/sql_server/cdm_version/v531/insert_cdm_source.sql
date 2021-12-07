bb5a9962307ab1f91512f9c7123f824731a5e1f6
Frank DeFalco <fdefalco@its.jnj.com>
2021-12-07 08:10:03 -0500

    Update insert_cdm_source.sql
    updates for 5.3 cdm source

--- inst/sql/sql_server/cdm_version/v531/insert_cdm_source.sql
+++ inst/sql/sql_server/cdm_version/v531/insert_cdm_source.sql
@@ -9,17 +9,17 @@ source_release_date,
 cdm_release_date,
 cdm_version,
 vocabulary_version
-) 
+)
 select
-'Synthea synthetic health database',
-'Synthea',
-'OHDSI Community',
-'SyntheaTM is a Synthetic Patient Population Simulator. The goal is to output synthetic, realistic (but not real), patient data and associated health records in a variety of formats.',
+'@cdm_source_name',
+'@cdm_source_abbreviation',
+'@cdm_holder',
+'@source_description',
 'https://synthetichealth.github.io/synthea/',
 'https://github.com/OHDSI/ETL-Synthea',
 getdate(), -- NB: Set this value to the day the source data was pulled
-getdate(), 
-'v5.3',
+getdate(),
+'@cdm_version',
 vocabulary_version
-from @cdm_schema.vocabulary 
+from @cdm_schema.vocabulary
 where vocabulary_id = 'None';
