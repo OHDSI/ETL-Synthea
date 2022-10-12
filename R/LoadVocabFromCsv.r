e1ec54aed36ef7a46f82656ed6d9c0ea912f2715
Iain <25081046+Iain-S@users.noreply.github.com>
2022-10-12 15:17:28 +0100

    Fix R CMD check imports warning

--- R/LoadVocabFromCsv.r
+++ R/LoadVocabFromCsv.r
@@ -62,7 +62,7 @@ LoadVocabFromCsv <- function (connectionDetails, cdmSchema, vocabFileLoc, bulkLo
 
 		if (tolower(csv) == "drug_strength.csv") {
 				vocabTable <- vocabTable %>%
-					mutate_at(vars('amount_value','amount_unit_concept_id','numerator_value','numerator_unit_concept_id','denominator_value','denominator_unit_concept_id', 'box_size'), ~tidyr::replace_na(., 0))
+					mutate_at(vars('amount_value','amount_unit_concept_id','numerator_value','numerator_unit_concept_id','denominator_value','denominator_unit_concept_id', 'box_size'), ~replace(., is.na(.), 0))
 		}
 
 		chunkSize <- 1e7
